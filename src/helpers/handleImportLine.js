30587fabefac3deab3463975455665471cae6c52
Milena <milena.delfini@gmail.com>
2020-06-05 02:21:54 +0200

    Fix missing arg into handleImportLine

--- src/helpers/handleImportLine.js
+++ src/helpers/handleImportLine.js
@@ -2,7 +2,7 @@ import get from "lodash/get";
 import isString from "lodash/isString";
 
 export const handleImportLine = (line) => {
-  if (!line || !isString) return null;
+  if (!line || !isString(line)) return null;
 
   const importLineRegex = /(?<import>import.*)?(?<from>}?\s*from[\s*|\n*])(?<path>.*)/;
   const matchLine = line.match(importLineRegex);
