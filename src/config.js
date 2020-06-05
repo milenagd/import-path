302bf6dd4565be8b0b973b4f50498685da9baee1
Milena <milena.delfini@gmail.com>
2020-06-05 02:16:57 +0200

    Update list of files to ignore

--- src/config.js
+++ src/config.js
@@ -1,9 +1,12 @@
 export const config = {
   ignoreList: [
     "node_modules",
+    "helpers",
+    "config.js",
     "package.json",
     "package-lock.json",
     "README.md",
+    "server.js",
     "x-backup.js",
   ],
   ignoreHiddenFiles: /^\..*$/,
