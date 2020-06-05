aeb4ac95a0a3ab3454f15183208d4ff6babfa3d6
Milena <milena.delfini@gmail.com>
2020-06-05 02:19:30 +0200

    Add feedback and stop process

--- src/index.js
+++ src/index.js
@@ -5,5 +5,8 @@ export const transformImportFile = () => {
   const inputRoot = process.argv[2] || "";
   const directoryPath = path.join(__dirname, "../");
   const root = path.resolve(inputRoot);
+  console.log(`Reading...`);
   readFilesByDir(directoryPath, root);
+  console.log("Completed");
+  process.exit();
 };
